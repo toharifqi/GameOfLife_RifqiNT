96d60992f329e6df1dec4333d85cba4f74d21a92
rifqi.t.interns <rifqi.t.interns@aux.gojek.com>
2021-09-19 10:44:41 +0700

    Added method printGrid()

--- src/com/rifqi/gameoflife/Main.java
+++ src/com/rifqi/gameoflife/Main.java
@@ -1,4 +1,48 @@
 package com.rifqi.gameoflife;
 
 public class Main {
+    int gridWidth;
+    int gridHeight;
+    int[][] grid;
+
+    public Main(int gridWidth, int gridHeight) {
+        this.gridHeight = gridHeight;
+        this.gridWidth = gridWidth;
+
+        this.grid = new int[gridWidth][gridHeight];
+    }
+
+    public void whenTopOrBottom(int y, StringBuilder line) {
+        if (y == 0 || y == gridHeight + 1) {
+            line.append(" _");
+        } else {
+            line.append(" |");
+        }
+    }
+
+    public void printGrid() {
+        for (int y = 0; y < gridHeight + 2; y++) {
+            StringBuilder line = new StringBuilder();
+            whenTopOrBottom(y, line);
+            for (int x = 0; x < gridWidth; x++) {
+                if (y == 0 || y == gridHeight + 2) {
+                    line.append(" _");
+                } else {
+                    if (grid[x][y - 1] == 0) {
+                        line.append(" .");
+                    } else {
+                        line.append(" *");
+                    }
+                }
+            }
+            whenTopOrBottom(y, line);
+            System.out.println(line);
+        }
+    }
+
+    public void setAlive(int[][] grid, int x, int y){
+
+    }
+
+
 }
